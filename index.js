84c55337df2e4c3b3eea6725a12b38fe826a374e
Vjs Pranavasri <pranavasri@live.in>
2022-09-29 12:01:46 +0530

    Add lambda for serving raw data

---
+++ index.js
@@ -0,0 +1,52 @@
+const AWS = require("aws-sdk");
+
+const documentClient = new AWS.DynamoDB.DocumentClient();
+
+const tableName = "content";
+
+exports.handler = async(event) => {
+    const params = {
+        TableName: tableName,
+        KeyConditionExpression: 'id = :hashKey',
+        //ProjectionExpression: "#id, #data, #url",
+        ExpressionAttributeValues: {
+            ':hashKey': event.pathParameters.id,
+        },
+        // ExpressionAttributeNames: {
+        //     "#data": "data",
+        //     "#id": "id",
+        //     "#url": "url"
+        // },
+    };
+
+    var data = await documentClient.query(params).promise();
+
+    if (!data.Items.length) {
+        return {
+            statusCode: 404,
+            headers: {
+                'Access-Control-Allow-Origin': '*'
+            },
+            body: JSON.stringify({ error: "Not found" })
+        };
+    }
+    
+    if (event.headers.buid == data.Items[0].buid) {
+        data.Items[0].edit = true;
+    } else {
+        data.Items[0].edit = false;
+    }
+    
+    delete data.Items[0].buid;
+    
+    const response = {
+        statusCode: 200,
+        headers: {
+            'content-type': 'text/plain',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, buid'
+        },
+        body: data.Items[0].data
+    };
+    return response;
+};
